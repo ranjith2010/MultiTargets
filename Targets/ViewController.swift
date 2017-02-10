f581909e7a95b877811e45975b32e343054f1d77
ranjith@f22labs.com <ranjith@f22labs.com>
2017-02-10 11:57:15 +0530

    Initial Commit

---
+++ Targets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Targets
+//
+//  Created by Ranjith Kumar on 10/02/2017.
+//  Copyright © 2017 F22Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
